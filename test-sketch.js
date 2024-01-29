a69d39a77f8cbdc37c536e289558ddf3c95a8b23
K6221 <103971204+K6221@users.noreply.github.com>
2024-01-29 10:47:33 -0800

    Update test-sketch.js

--- test-sketch.js
+++ test-sketch.js
@@ -1,32 +1,92 @@
-// drawn from https://p5js.org/examples/color-color-wheel.html
+// drawn from https://p5js.org/examples/simulate-game-of-life.html
+
+let w;
+let columns;
+let rows;
+let board;
+let next;
+
 function setup() {
-    createCanvas(400, 400) ;
-    colorMode(HSB);
-    
-    //setting angle mode to degrees 
-    //to aid in positioning of circles
-    angleMode(DEGREES);
+  // Set simulation framerate to 10 to avoid flickering
+  frameRate(10);
+  createCanvas(720, 400);
+  w = 20;
+  // Calculate columns and rows
+  columns = floor(width / w);
+  rows = floor(height / w);
+  // Wacky way to make a 2D array is JS
+  board = new Array(columns);
+  for (let i = 0; i < columns; i++) {
+    board[i] = new Array(rows);
   }
-  
-  function draw() {
-    background (220)
-    noStroke();
-  
-    //loop steps through 36 times drawing 
-    //a circle with a separate hue each time
-    
-    for (let i = 0;i<36;i++){
-      
-    //determines position of circles
-      x = 200 + 100*cos(i*10);
-      y = 200 + 100*sin(i*10);
-    
-    //i * 10 allows to cover the entire hue range
-    //the first circle has a hue of 0*10 = 0
-    // last circle has has a hue of 35*10 = 350
-    // hue of 0 and 360 are identical
-      
-      fill(i*10,100,100);
-      circle(x,y,50);
+  // Going to use multiple 2D arrays and swap them
+  next = new Array(columns);
+  for (i = 0; i < columns; i++) {
+    next[i] = new Array(rows);
+  }
+  init();
+}
+
+function draw() {
+  background(255);
+  generate();
+  for ( let i = 0; i < columns;i++) {
+    for ( let j = 0; j < rows;j++) {
+      if ((board[i][j] == 1)) fill(0);
+      else fill(255);
+      stroke(0);
+      rect(i * w, j * w, w-1, w-1);
     }
   }
+
+}
+
+// reset board when mouse is pressed
+function mousePressed() {
+  init();
+}
+
+// Fill board randomly
+function init() {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      // Lining the edges with 0s
+      if (i == 0 || j == 0 || i == columns-1 || j == rows-1) board[i][j] = 0;
+      // Filling the rest randomly
+      else board[i][j] = floor(random(2));
+      next[i][j] = 0;
+    }
+  }
+}
+
+// The process of creating the new generation
+function generate() {
+
+  // Loop through every spot in our 2D array and check spots neighbors
+  for (let x = 1; x < columns - 1; x++) {
+    for (let y = 1; y < rows - 1; y++) {
+      // Add up all the states in a 3x3 surrounding grid
+      let neighbors = 0;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          neighbors += board[x+i][y+j];
+        }
+      }
+
+      // A little trick to subtract the current cell's state since
+      // we added it in the above loop
+      neighbors -= board[x][y];
+      // Rules of Life
+      if      ((board[x][y] == 1) && (neighbors <  2)) next[x][y] = 0;           // Loneliness
+      else if ((board[x][y] == 1) && (neighbors >  3)) next[x][y] = 0;           // Overpopulation
+      else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1;           // Reproduction
+      else                                             next[x][y] = board[x][y]; // Stasis
+    }
+  }
+
+  // Swap!
+  let temp = board;
+  board = next;
+  next = temp;
+}
+
